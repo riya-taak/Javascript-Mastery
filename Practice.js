73ce09ad4a62cc5b569f217dfdbf6c59dba7a748
Rahul-taak <rtaak786@gmail.com>
2025-07-04 17:02:55 +0530

    first stage

---
+++ Practice.js
@@ -0,0 +1,47 @@
+var a = 12;
+console.log(a + " Riya");
+
+// for loop
+
+// while loop
+
+// do while
+
+// for of = []
+
+let obj1 = {
+    name: 'Riya',
+    age: 20,
+    roll_no: 18,
+    married: false
+}
+
+let obj2 = {
+    name: 'Riya',
+    age: 20,
+    gender: 'female',
+}
+
+const arr = [obj1, obj2];
+for(let s of arr){
+    listAllValues(s)
+    console.log('-'.repeat(30))
+}
+
+// for in = {}
+
+function listAllValues(myObj){   
+    for(let ob in myObj){
+        console.log(ob + ' ---> ' + myObj[ob]);
+    }
+}
+
+// forEach 
+
+// map | filter | reduce
+
+let a =Riya;
+let b =Priya;
+let c = `${a} and ${b} are sisters `
+console.log("c");
+
