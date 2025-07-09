d54671af1f833eb5d2c45b5e4b82481ada6c8235
Riya taak <taakriya64@gmail.com>
2025-07-09 13:35:05 +0700

    Array_print

--- array_practice.js
+++ array_practice.js
@@ -1,1 +1,13 @@
-// This file is used array practice questions
+// This file is used array practice questions 
+let marks_class_12 = [78,39,49,28,true,"NOt present"]
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6]) //wil be undefined because index 6 does not exits
+console.log("The lengtth of marks_class_12 is",marks_class_12.length)
+marks_class_12[6] = 100 //Adding a new value to the array
+marks_class_12[0] = 189 //changing the value of an array
+consloe.log (marks_class_12) 
